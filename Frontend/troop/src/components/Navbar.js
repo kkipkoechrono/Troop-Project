1603f11d4e4a76fef2e1b8bcbab433a6acccb3a7
quincy mwangi <mwangiquincy77@gmail.com>
2024-10-18 15:18:57 +0300

    added content

---
+++ Frontend/troop/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className='nav'>
+     <h1>Troop Tracker</h1>
+    </div>
+  )
+}
+
+export default Navbar
