0e6e9285927abb494713fc3f45fea0b6b55090be
quincy mwangi <mwangiquincy77@gmail.com>
2024-10-18 16:26:54 +0300

    added navbar

--- Frontend/troop/src/components/Navbar.js
+++ Frontend/troop/src/components/Navbar.js
@@ -3,9 +3,18 @@ import './Navbar.css'
 
 const Navbar = () => {
   return (
+    <>
     <div className='nav'>
      <h1>Troop Tracker</h1>
+
+    <div className='anchors'>
+      <a href='#home'>Home</a>
+      <a href='#services'>Units</a>
+      <a href='#about'>About</a>
+      <a href='#contact'>Contact</a>
     </div>
+    </div>
+    </>
   )
 }
 
