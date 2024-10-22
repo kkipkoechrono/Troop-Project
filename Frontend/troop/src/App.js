054f62fbe911cd29a937dac4bee39866ae7e0d20
Omwansam <omwansamarnold@gmail.com>
2024-10-22 07:44:19 +0300

    Today Changes

--- Frontend/troop/src/App.js
+++ Frontend/troop/src/App.js
@@ -29,7 +29,7 @@ const App = () => {
         <Route path='/register' element={<Register/>}/>
         <Route path="/loginform" element={<LoginForm />} />
       </Routes>
-    </Router> 
+    </Router>
   );
 }
 
--- Frontend/troop/src/components/Navbar.js
+++ Frontend/troop/src/components/Navbar.js
@@ -7,23 +7,23 @@ const Navbar = () => {
   return (
     <>
     <div className='nav'>
-     <h1>Troop Tracker</h1>
+      <h1>Troop Tracker</h1>
 
-    <div className='anchors'>
-      <Link to='/'>Home</Link>
+      <div className='anchors'>
+        <Link to='/'>Home</Link>
      <a className='dropdown' href='#services'>Units <i class="fa-solid fa-angle-down"></i>
-       <div className='content'>
-         <Link to='/army'>Army</Link>
-         <Link to='/airforce'>Air Force</Link>
-         <Link to='/navy'>Navy</Link>
-         <Link to='/marines'>Marines</Link>
+          <div className='content'>
+            <Link to='/army'>Army</Link>
+            <Link to='/airforce'>Air Force</Link>
+            <Link to='/navy'>Navy</Link>
+            <Link to='/marines'>Marines</Link>
         
-       </div>
+          </div>
      
-     </a>
-     <Link to='/about'>About</Link>
-     <Link className='btn' to='/login'>Login/Signup</Link>
-    </div>
+        </a>
+        <Link to='/about'>About</Link>
+          <Link className='btn' to='/login'>Login/Signup</Link>
+      </div>
     </div>
     </>
   )
