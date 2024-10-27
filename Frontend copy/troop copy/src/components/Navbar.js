c8b90c498aa2df435a2782a24b82dda8943e65fd
Omwansam <omwansamarnold@gmail.com>
2024-10-27 13:43:06 +0300

    commit

--- Frontend copy/troop copy/src/components/Navbar.js
+++ Frontend copy/troop copy/src/components/Navbar.js
@@ -11,12 +11,7 @@ const Navbar = () => {
       <div className='anchors'>
         <Link to='/'>Home</Link>
      <a className='dropdown' href='#services'>Units <i class="fa-solid fa-angle-down"></i>
-<<<<<<< HEAD:Frontend/troop/src/components/Navbar.js
-          <div className='content'>
-            <Link to='/army'>Army</Link>
-            <Link to='/airforce'>Air Force</Link>
-            <Link to='/navy'>Navy</Link>
-            <Link to='/marines'>Marines</Link>
+
 =======
        <div className='content'>
          <Link to='/army'>Army</Link>
@@ -24,21 +19,16 @@ const Navbar = () => {
          <Link to='/navy'>Navy</Link>
          <Link to='/marines'>Marines</Link>
          
->>>>>>> 972135335a243c470ba57a57aae424ce36bbf519:Frontend copy/troop copy/src/components/Navbar.js
+
         
           </div>
      
-<<<<<<< HEAD:Frontend/troop/src/components/Navbar.js
-        </a>
-        <Link to='/about'>About</Link>
-          <Link className='btn' to='/login'>Login/Signup</Link>
-      </div>
-=======
+
      </a>
      <Link to='/about'>About</Link>
       <Link className='btn' to='/login'>Login/Signup</Link>
     </div>
->>>>>>> 972135335a243c470ba57a57aae424ce36bbf519:Frontend copy/troop copy/src/components/Navbar.js
+
     </div>
     </>
   )
